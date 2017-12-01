74950a0fb3fdf19d2a37d366fa5275f3b8f740a0
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-12-01 18:32:38 -0500

    Finished the hopefully-working version of the latest-versions script.

--- latest-versions/main.pl
+++ latest-versions/main.pl
@@ -8,11 +8,98 @@ use strict;
 my $hme;
 my $tmpdir;
 my $tmpprefix = 'tmp_shallow_git';
+my @rmnargs = ();
+
+# Identify the branch:
+my $brnc_set = 0;
+my $brnc_at;
+
+# Identify the repo
+my $repo_set = 0;
+my $repo_at;
+
+my $my_tmp_dir_lc;
+my $my_tmp_dir_gl;
+
+# ################################# #
+# ##  END VARIABLE DECLARATIONS  ## #
+# ################################# #
 
 $hme = $ENV{'HOME'};
 $tmpdir = ( $hme . '/tmp' );
 system('mkdir','-p',$tmpdir);
 
+sub goodry {
+  my $lc_a;
+  $lc_a = @_;
+  return ( $lc_a > 0.5 );
+}
+
+sub getargo {
+  my $lc_a;
+  $lc_a = '';
+  if ( &goodry(@rmnargs) ) { $lc_a = shift(@rmnargs); }
+  return $lc_a;
+}
+
+@rmnargs = @ARGV;
+while ( &goodry(@rmnargs) ) { &eacharg(); }
+sub eacharg {
+  my $lc_opto;
+  $lc_opto = &getargo();
+
+  if ( $lc_opto = '-brnc' )
+  {
+    $brnc_at = &getargo();
+    if ( $brnc_at ne '' ) { $brnc_set = 10; }
+    return;
+  }
+
+  if ( $lc_opto eq '-repo' )
+  {
+    $repo_at = &getargo();
+    if ( $repo_at ne '' ) { $repo_set = 10; }
+    return;
+  }
+
+  die ("\nNO SUCH OPTION: " . $lc_opto . ":\n\n");
+}
+
+
+# Now we identify the repo if the command-line hasnt:
+if ( $repo_set < 5 )
+{
+  $repo_at = `git remote get-url origin`;
+  chomp($repo_at);
+  if ( $repo_at ne '' ) { $repo_set = 10; }
+}
+
+
+# Now we identify the branch if the command-line hasnt:
+if ( $brnc_set < 5 )
+{
+  $brnc_at = `git rev-parse --abbrev-ref HEAD`;
+  chomp($brnc_at);
+  if ( $brnc_at ne '' ) { $brnc_set = 10; }
+}
+
+
+# Do not proceed any further if any of the required
+# parameters wasn't set:
+if ( $repo_set < 5 ) { die "\nNo remote repository URL identified.\n\n"; }
+if ( $brnc_set < 5 ) { die "\nNo rempository branch identified.\n\n"; }
+
+# Output the parameter info
+print('info:repo:' . &aline($repo_at) . "\n");
+print('info:brnc:' . &aline($brnc_at) . "\n");
+
+sub aline {
+  # Currently this function just returns it's one argument - but
+  # eventually, it will be re-implemented to break that argument
+  # into a form that is guaranteed to fit neatly into the line
+  # structure of the output.
+  return $_[0];
+}
 
 
 sub bsc {
@@ -32,8 +119,10 @@ sub bsc {
 }
 
 # Now we clean out any gunk that shouldn't be there.
-# Basically, any temporary program left over an hour
+# Basically, any temporary program left over six hours
 # by a script like this one.
+#   This part is to be referred to as the "retero
+# cleanup".
 {
   my $lc_a;
   my $lc_b;
@@ -49,13 +138,73 @@ sub bsc {
     @lc_e = split(quotemeta('-'),$lc_d);
     if ( $lc_e[0] eq $tmpprefix )
     {
-      if ( ( $lc_e[1] + ( 60 * 60 ) ) < time() )
+      if ( ( $lc_e[1] + ( 6 * 60 * 60 ) ) < time() )
       {
-        system('rm','-rf',($tmpprefix . '/' . $lc_d));
+        system('rm','-rf',($tmpdir . '/' . $lc_d));
       }
     }
   }
 }
 
+$my_tmp_dir_lc = $tmpprefix . '-' . time() . '-' . $$;
+$my_tmp_dir_gl = $tmpdir . '/' . $my_tmp_dir_lc;
+
+
+# Now we do the cloning.
+{
+  my $lc_cm;
+  $lc_cm = 'git clone ' . &bsc($repo_at) . ' ' . &bsc($my_tmp_dir_gl);
+  $lc_cm .= ' --depth 1';
+  $lc_cm .= ' -b ' . &bsc($brnc_at);
+
+  # Now we make sure that there will be no clone directory if
+  # the operation fails.
+  $lc_cm .= ' || rm -rf ' . &bsc($my_tmp_dir_gl);
+
+  # Now we redirect the raw output so as to not confuse things:
+  $lc_cm = '( ' . $lc_cm . ' )';
+  $lc_cm .= ' > /dev/null 2> /dev/null';
+
+  system($lc_cm);
+  #system('echo',("\n" . $lc_cm . "\n"));
+}
+
+# We exit the program if the cloning operation failed.
+if ( ! ( -d $my_tmp_dir_gl ) )
+{
+  die '\nCould not obtain the current Special Versions list.\n\n';
+}
+
+# Now we do the catting.
+{
+  my $lc_cm;
+  my $lc_raw;
+  my @lc_list;
+  my $lc_each;
+  $lc_cm = 'cat ' . &bsc($my_tmp_dir_gl . '/special-versions.dat');
+  $lc_cm .= ' 2> /dev/null';
+
+  $lc_raw = `$lc_cm`;
+  @lc_list = split(/\n/,$lc_raw);
+  foreach $lc_each (@lc_list)
+  {
+    if ( $lc_each ne '' ) { print('rcd:' . $lc_each . "\n"); }
+  }
+  #system('echo',("\n" . $lc_cm . "\n"));
+}
+
+# And finally - we identify the current Revision ID of the remote repo.
+{
+  my $lc_cm;
+  my $lc_ret;
+  $lc_cm = 'cd ' . &bsc($my_tmp_dir_gl);
+  $lc_cm .= ' && git rev-parse HEAD';
+  $lc_ret = `$lc_cm`; chomp($lc_ret);
+  print('current:' . $lc_ret . "\n");
+}
+
+# And now we clean-up. (The retero-cleaning is a plan B, not a plan A.)
+print "done:\n";
+system('rm','-rf',$my_tmp_dir_gl);
 
 
